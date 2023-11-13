257b7308381aee62e9add99f2e3943ff1a58117b
Rafatlne <ashikrafat.lne@gmail.com>
2023-11-13 14:53:21 +0600

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,48 @@
+AUTHOR = 'baps-oj@org.com'
+SITENAME = 'baps-engineering-team'
+SITEURL = 'http://127.0.0.1:8000/'
+
+PATH = "content"
+
+TIMEZONE = 'Asia/Dhaka'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+THEME = 'theme/papyrus'
+THEME_STATIC_DIR="theme/papyrus/static"
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+DISPLAY_PAGES_ON_MENU = True
+DIRECT_TEMPLATES = (('index', 'search', 'tags', 'categories', 'archives',))
+PAGINATED_TEMPLATES = {'index': None, 'tag': None, 'category': None, 'author': None, 'archives': 24,}
+
+# PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['readtime', 'neighbors']
+
+# Site search plugin
+# SEARCH_MODE = "output"
+# SEARCH_HTML_SELECTOR = "main"
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
